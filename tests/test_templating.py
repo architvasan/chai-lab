408811567ac824643a0dacf0f65d15faf440e790
Archit <akvasan2@illinois.edu>
2025-01-28 20:30:23 +0000

    mods

--- tests/test_templating.py
+++ tests/test_templating.py
@@ -34,6 +34,41 @@ def test(
                             )
     return
 
+def test_subsample(
+        template_pdbs: str,
+        fasta_file: Path,
+        output_dir: Path,
+        use_esm_embeddings: bool = True,
+        use_msa_server: bool = False,
+        msa_server_url: str = "https://api.colabfold.com",
+        msa_directory: Path = None,
+        constraint_path: Path = None,
+        esm_device: torch.device = torch.device("cpu"),
+    ):
+
+    cont = chai1.make_all_atom_feature_context(
+                fasta_file,
+                output_dir,
+                use_esm_embeddings,
+                use_msa_server,
+                msa_server_url,
+                msa_directory,
+                constraint_path,
+                template_pdbs,
+                esm_device,
+            ) 
+    #print(dir(cont))
+    print(f"temp cont: {cont.template_context}")
+    print(f"emb cont: {dir(cont.embedding_context)}")
+    print(f"struc cont: {cont.structure_context}")
+    import sys
+    sys.exit()
+    chai1.run_folding_on_context(cont,
+                           output_dir
+                            )
+    return
+
+
 if __name__=="__main__":
     import argparse
 
@@ -41,7 +76,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Which test to run?")
     
     # Add arguments
-    parser.add_argument("-t", "--test", type=str, help="Test type: monomer, complex")
+    parser.add_argument("-t", "--test", type=str, help="Test type: monomer, complex, subsample")
     
     # Parse arguments
     args = parser.parse_args()
@@ -59,7 +94,12 @@ if __name__=="__main__":
             Path('input_tests/nmnat2_fbx.fasta'),
             Path('output_tests/templating_nmnat2_fbx')
             )
-
+    elif args.test == "subsample":
+        test_subsample(
+            'input_tests/meta3.pdb',
+            Path('input_tests/nmnat2_fbx.fasta'),
+            Path('output_tests/templating_nmnat2_fbx_sub')
+        )
 
 #test(
 #    "None",
